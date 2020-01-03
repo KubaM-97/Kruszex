68ba86439d883424743bf8492c0da9107ed8c450
KubaM-97 <jakubm16@interia.pl>
2020-01-03 16:56:21 +0100

    gold ajax silver

--- script.js
+++ script.js
@@ -4,13 +4,29 @@ $(function () {
         const url = this.href;
         $(".content").remove();
         const ids = this.id;
-        if(ids){
+        if (ids) {
             $("main").load(url + " .content #" + ids).hide().fadeIn(1000);
-        }
-        else{
+        } else {
             $("main").load(url + " .content").hide().fadeIn(1000);
         }
     });
+    $(document).ajaxComplete(function () {
+
+        const $rows = $("tbody tr");
+        $("#countryNumber").on("input", function () {
+            const $ile = $("select").val();
+            const $ileile = parseInt($ile);
+            $("tbody").empty();
+            $tableContent = "";
+
+            for (let i = 0; i <= $ileile - 1; i++) {
+                $tableContent = $rows[i];
+                $("tbody").append($tableContent);
+            }
+        });
+
+    });
+
 });
 //fetch('https://jsonplaceholder.typicode.com/todos/1')
 //  .then(response => response.json())
