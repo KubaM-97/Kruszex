720e02651b0b9148103900332f760db8e383dd99
KubaM-97 <jakubm16@interia.pl>
2021-01-29 16:17:20 +0100

    new API, uglifyjs -> terser

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,9 @@
-require("!style-loader!css-loader!./style.css")
-require('file-loader?name=[name].[ext]!./index.html');
-require('file-loader?name=[name].[ext]!./gold.html');
-require('file-loader?name=[name].[ext]!./silver.html');
-require('file-loader?name=[name].[ext]!./tips.html');
-require('file-loader?name=[name].[ext]!./contact.html');
-import './script.js'
-import './fetch.js'
+require("!style-loader!css-loader!../css/style.css")
+require('file-loader?name=[name].[ext]!../index.html');
+require('file-loader?name=[name].[ext]!../gold.html');
+require('file-loader?name=[name].[ext]!../silver.html');
+require('file-loader?name=[name].[ext]!../tips.html');
+require('file-loader?name=[name].[ext]!../contact.html');
+import './different_functions.js'
+import './fetch_price.js'
 import 'jquery'
-// import './fontello/**/*'
-// import "./gold.html"
-// import "./silver.html"
-// import "./tips.html"
-// import "./contact.html"
--- webpack.config.js
+++ webpack.config.js
@@ -1,112 +1,62 @@
-
 const path = require("path");
 const webpack = require("webpack");
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {
-    CleanWebpackPlugin
-} = require("clean-webpack-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const extractPlugin = new ExtractTextPlugin({
-    filename: 'main.css'
-});
-const FontelloPlugin = require("fontello-webpack-plugin");
- 
-module.export = {
-    entry: [{
-        app: "./src/js/app.js"
-    }],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: 'bundle.js',
-        publicPath: "/dist"
-    },
-    module: {
-        rules: [{
-                test: /\.js$/,
-                use: [{
-                    loader: 'babel-loader',
-                    exclude: /node_modules/,
-                    options: {
-                        presets: ["env"]
-                    }
-                }]
-            },
-            {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                use: ["html-loader"]
-            },
-            {
-                test: /\.(png|jpg|gif|jfif)$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192
-                    }
-                }]
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ]
-            },
-            {
-                test: /\.html$/,
-                exclude: path.resolve(__dirname, "src/index.html"),
-                use: [{
-                    loader: "file-loader",
-                    options: {
-                        name: "[name].[ext]"
-                    }
-                }]
-            },
-                    ]
-    },
-    optimization: {
-        minimizer: [
-            new UglifyJsPlugin({
-                extractComments: false
-            })
-        ],
-    },
-    plugins: [
-        extractPlugin,
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: "./src/index.html"
-        }),
-        // new HtmlWebpackPlugin({
-        //     filename: "gold.html",
-        //     template: "./src/gold.html",
-        //     chunks: ["app"]
-        // }),
-        // new HtmlWebpackPlugin({
-        //     filename: "silver.html",
-        //     template: "./src/silver.html",
-        //     chunks: ["app"]
-        // }),
-        // new HtmlWebpackPlugin({
-        //     filename: "tips.html",
-        //     template: "./src/tips.html",
-        //     chunks: ["app"]
-        // }),
-        // new HtmlWebpackPlugin({
-        //     filename: "contact.html",
-        //     template: "./src/contact.html",
-        //     chunks: ["app"]
-        // }),
-        new CleanWebpackPlugin(),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jquery: "jquery"
-        }),
-        new FontelloPlugin()
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
+module.exports = {
+  entry: { 
+    index: path.resolve(__dirname, "src/js", "app.js") 
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: 'bundle.js',
+    publicPath: '',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
     ]
-
-}
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      filename: 'gold.html',
+      template: 'src/gold.html'
+    }),
+    new HtmlWebpackPlugin({ 
+      filename: 'silver.html',
+      template: 'src/silver.html'
+    }),
+    new HtmlWebpackPlugin({ 
+      filename: 'tips.html',
+      template: 'src/tips.html'
+    }),
+    new HtmlWebpackPlugin({ 
+      filename: 'contact.html',
+      template: 'src/contact.html'
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jquery: "jquery"
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/img", to: "img" }
+      ],
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
